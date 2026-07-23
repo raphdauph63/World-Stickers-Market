42a7451f1f3425b7cfb81798773d12f999021a0b
raphdauph63 <dolphinsafe2@gmail.com>
2026-07-23 15:01:08 +0100

    Add teams data with country names and flags

---
+++ components/components/types/data/teams.ts
@@ -0,0 +1,27 @@
+export const teams=[
+
+{
+id:"france",
+name:"France",
+flag:"🇫🇷"
+},
+
+{
+id:"argentine",
+name:"Argentine",
+flag:"🇦🇷"
+},
+
+{
+id:"bresil",
+name:"Brésil",
+flag:"🇧🇷"
+},
+
+{
+id:"espagne",
+name:"Espagne",
+flag:"🇪🇸"
+}
+
+]
