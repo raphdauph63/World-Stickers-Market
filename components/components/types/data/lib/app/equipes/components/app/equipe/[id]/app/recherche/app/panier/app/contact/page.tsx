f57802a455420e6857c17c87473bcef8d8f1af29
raphdauph63 <dolphinsafe2@gmail.com>
2026-07-23 15:06:32 +0100

    Create page.tsx

---
+++ components/components/types/data/lib/app/equipes/components/app/equipe/[id]/app/recherche/app/panier/app/contact/page.tsx
@@ -0,0 +1,31 @@
+export default function Contact(){
+
+return(
+
+<main className="container">
+
+<h1>
+
+Contact
+
+</h1>
+
+<p>
+
+Collectionneurs français spécialisés dans les cartes Panini.
+
+</p>
+
+<p>
+
+Email :
+
+contact@worldstickersmarket.fr
+
+</p>
+
+</main>
+
+)
+
+}
